684e9747fb3d1e114968e1ac33e716671f6d3976
dstoffels <dan.stoffels@gmail.com>
2022-03-08 10:30:34 -0600

    fix: added port to app.listen()

--- App.js
+++ App.js
@@ -5,12 +5,9 @@ const app = express();
 const APP_PORT = process.env.APP_PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!'),
-		() => {
-			console.log(`Running at at port ${APP_PORT}`);
-		};
+	res.send('Hello World!');
 });
 
-app.listen(() => {
+app.listen(APP_PORT, () => {
 	console.log(`Running app at port ${APP_PORT}`);
 });
